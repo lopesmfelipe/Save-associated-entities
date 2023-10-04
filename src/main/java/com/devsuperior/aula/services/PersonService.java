73e2ac7a6cb88e7a8ae88c733c623fc02f4334d1
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-10-04 00:59:56 -0400

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
@@ -35,5 +36,25 @@ public class PersonService {
         entity = repository.save(entity);
 
         return new PersonDepartmentDTO(entity);
+    }
+
+    public PersonDTO insert(PersonDTO dto) {
+
+        Person entity = new Person();
+        entity.setName(dto.getName());
+        entity.setSalary(dto.getSalary());
+
+        // managed Entity
+        //Department dept = departmentRepository.getReferenceById(dto.getDepartmentId());
+
+        // PREVIOUS METHOD
+        Department dept = new Department();
+        dept.setId(dto.getDepartmentId());
+
+        entity.setDepartment(dept);
+
+        entity = repository.save(entity);
+
+        return new PersonDTO(entity);
     }
 }
